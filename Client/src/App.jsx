be36098b31618aeff78576f7a0893308c845e0fd
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-06-09 16:37:11 -0300

    ok

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 
 import "./App.css";
 import ClientHome from "./Views/client/ClientHome/ClientHome";
-import DashboardUser from "./components/Client/DashboardUser/DashboardUser";
 
 function App() {
   return (
---
+++ Client/src/Views/admin/AdminHome/AdminHome.jsx
@@ -0,0 +1,5 @@
+const AdminHome = ()=>{
+
+}
+
+export default AdminHome;
