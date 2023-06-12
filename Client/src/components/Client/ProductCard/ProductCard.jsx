e02b5adffa524cb662beaf7ed103c7aed9a6d581
marco <m_uno_p@hotmail.com>
2023-06-12 18:57:05 -0500

    Add product card

---
+++ Client/src/components/Client/ProductCard/ProductCard.jsx
@@ -0,0 +1,43 @@
+import styles from './ProductCard.module.css';
+import { Row, Col, Space, Card } from 'antd';
+import {
+	HeartOutlined, 
+	HeartFilled 
+} from '@ant-design/icons';
+import { useState } from 'react';
+
+const ProductCard = ({ id, e_product_type, photo, name, price }) => {
+	const [isFavorite, setIsFavorite] = useState(false);
+	const { Meta } = Card;
+
+	const onClick = () => {
+		setIsFavorite(!isFavorite);
+	};
+
+	return (
+		<Row justify='center' className={ styles.productCard__mainCardContainer }>
+		    <Col className={ styles.productCard__secondaryCardContainer }>
+		        <Row justify='end' className={ styles.productCard__heart }>
+		            <button onClick={ onClick } className={ styles.productCard__heartButton }>
+		                {
+		                	isFavorite ? (<HeartFilled />) : (<HeartOutlined />)
+		                }
+		            </button>
+		    	</Row>
+				<Card
+				    hoverable
+				    style={{ width: 240 }}
+				    cover={<img alt={ name } src={ photo } />}
+				>
+				    <div>{ e_product_type }</div>
+				    <Meta 
+				        title={ name }
+				    />
+				    <div>$ { price }</div>
+				</Card>
+			</Col>
+		</Row>
+	);
+};
+
+export default ProductCard;
