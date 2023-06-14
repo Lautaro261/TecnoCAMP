7643207b17044ec81b07ab662d464309bffd8afd
[Tatsumi0720] <[escobandress@gmail.com]>
2023-06-14 15:11:57 -0500

    finish

---
+++ Api/src/handlers/client/GET/handlerFilterBrand.js
@@ -0,0 +1,13 @@
+const getBrandProducts = require("../../../controllers/client/GET/getBrandProducts");
+
+const handlerFilterBrand= async (req, res) => {
+    const id = req.body.id;
+    try {
+        const filterBrand = await getBrandProducts(id);
+        res.status(200).json(filterBrand);
+    } catch (error) {
+        res.status(404).json( { message: error.message });
+    }
+}
+
+module.exports = handlerFilterBrand;
