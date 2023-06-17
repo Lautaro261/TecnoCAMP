e119f7b94fff095d715aa02e5ff217e82d056343
Andres Garcia <andresfgarciac96@gmail.com>
2023-06-17 14:59:51 -0500

    order code routes

--- Api/src/controllers/client/GET/getBrandProducts.js
+++ Api/src/controllers/client/GET/getBrandProducts.js
@@ -1,14 +1,13 @@
 const { Product } = require("../../../db");
 
 const getBrandProducts = async (id) => {
-    
-    const filterBrand = await Product.findAll( {where: {brandId: id}});
+  const filterBrand = await Product.findAll({ where: { brandId: id } });
 
-    if (!filterBrand) {
-        return { message: "No existe productos relacionados con esa Categoria" };
-    };
+  if (!filterBrand) {
+    return { message: "No existe productos relacionados con esa Categoria" };
+  }
 
-    return filterBrand;
-}
+  return filterBrand;
+};
 
 module.exports = getBrandProducts;
