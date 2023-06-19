b5f2921e353d2a930332b0c3b8fab9368a865fe5
Camila Oyene <coyene63@gmail.com>
2023-06-19 15:23:38 -0300

    popConfirm para logout

--- Client/src/components/Client/DashboardUser/Icons/IconLogout.jsx
+++ Client/src/components/Client/DashboardUser/Icons/IconLogout.jsx
@@ -1,24 +1,59 @@
-import React from "react";
-import { LogoutOutlined } from '@ant-design/icons'
-import { Row, Col} from 'antd'
+import React, { useState } from "react";
+import { LogoutOutlined } from '@ant-design/icons';
+import { Row, Col, Popconfirm } from 'antd';
 import { useNavigate } from "react-router-dom";
 
-const IconLogout = () =>{
-     const navigate = useNavigate()
+const IconLogout = () => {
+    const navigate = useNavigate()
+    const [open, setOpen] = useState(false);
+    const [confirmLoading, setConfirmLoading] = useState(false);
+
     const handleLogout = () => {
         window.localStorage.removeItem('token')
         window.localStorage.removeItem('rol')
+
+        console.log('te deslogueaste')
         navigate('/home')
     }
 
-    return(
+    const showPopconfirm = () => {
+        setOpen(true);
+    };
+
+    const handleOk = () => {
+        setConfirmLoading(true);
+        handleLogout();
+        setTimeout(() => {
+            setOpen(false);
+            setConfirmLoading(false);
+        }, 2000);
+    };
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setOpen(false);
+    };
+
+
+    return (
         <Row >
-        <Col span={24}>
-            <button onClick={handleLogout} style={{ background: 'transparent', border: 'none', cursor: 'pointer'}}>
-                <LogoutOutlined style={{ fontSize: '28px', color: '#6699FF' }} />
-            </button>
-        </Col>
-    </Row>
+            <Col span={24}>
+                <Popconfirm
+                    title="Title"
+                    description="Open Popconfirm with async logic"
+                    open={open}
+                    onConfirm={handleOk}
+                    okButtonProps={{
+                        loading: confirmLoading,
+                    }}
+                    onCancel={handleCancel}
+                >
+                    <button onClick={showPopconfirm} style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
+                        <LogoutOutlined style={{ fontSize: '28px', color: '#6699FF' }} />
+                    </button>
+                </Popconfirm>
+            </Col>
+        </Row>
     )
 }
 
