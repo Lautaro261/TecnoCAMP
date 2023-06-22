ce8a7187bce8921b7f41bd2e714e0231d0760e42
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-06-22 16:26:17 -0300

    ok

--- Client/src/components/LogoutAuth/LogoutAuth.jsx
+++ Client/src/components/LogoutAuth/LogoutAuth.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useAuth0 }from '@auth0/auth0-react';
 import { LogoutOutlined } from '@ant-design/icons';
+import {Popconfirm , Row, Col} from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const LogoutAuth = () => {
 
@@ -30,6 +32,11 @@ const LogoutAuth = () => {
         },2000)
         handleLogoutAuth();
     }
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setOpen(false);
+    };
 
     return (
         <Row >
