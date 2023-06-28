25045fd190025f379dfde7fdf33b9cb96f21919f
Camila Oyene <coyene63@gmail.com>
2023-06-28 03:14:19 -0300

    dejo todo como estaba

--- Client/src/components/Client/ContainerAllProducts/ContainerAllProducts.jsx
+++ Client/src/components/Client/ContainerAllProducts/ContainerAllProducts.jsx
@@ -25,7 +25,7 @@ const ContainerAllProducts = () => {
         if (allProducts.length > 0) {
             dispatch(setFilteredProducts(allProducts));
         }
-    }, [allProducts]);
+    }, [allProducts, dispatch]);
 
     return (
         <div>
@@ -39,7 +39,7 @@ const ContainerAllProducts = () => {
                 <Row gutter={[16, 16]}>
                     {currentFilteredProducts.length ? currentFilteredProducts.map(product => {
                         return (
-                            <Col  xs={24} sm={12} md={8} lg={6} key={product.id}>
+                            <Col xs={24} sm={12} md={8} lg={6} key={product.id}>
                                 <ProductCard 
                                 e_product_type={product.e_product_type} 
                                 name={product.name} 
